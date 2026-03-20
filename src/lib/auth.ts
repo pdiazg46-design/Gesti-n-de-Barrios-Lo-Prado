cf7d76130d6e805957406bdd63646458aeed9e29
Pdiaz <pdiazg46@gmail.com>
2026-03-20 16:13:30 -0300

    fix(auth): Permitir account linking temporal para sortear CSRF Vercel

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,12 +6,16 @@ export const authOptions: NextAuthOptions = {
         GoogleProvider({
             clientId: (process.env.GOOGLE_CLIENT_ID || "").trim(),
             clientSecret: (process.env.GOOGLE_CLIENT_SECRET || "").trim(),
+            allowDangerousEmailAccountLinking: true,
         }),
     ],
     session: {
         strategy: "jwt",
     },
     callbacks: {
+        async signIn({ user, account, profile }) {
+            return true;
+        },
         async session({ session, token }) {
             if (session.user) {
                 (session.user as any).id = token.sub;
@@ -21,6 +25,5 @@ export const authOptions: NextAuthOptions = {
     },
     secret: (process.env.NEXTAUTH_SECRET || "lo-prado-secret-2026").trim(),
     debug: true,
-
 };
 
